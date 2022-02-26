92e1268827ee02c4d8daf5e9a35fa7018cf5d515
MDAddario <michael.lindner.daddario@gmail.com>
2022-02-25 22:31:36 -0500

    Outlines format of 3 columned website

--- otaku-visuals/src/App.js
+++ otaku-visuals/src/App.js
@@ -6,17 +6,20 @@ function App() {
       <header className="App-header">
         <img src="otaku_sayu_head.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          create your personalized otaku avatar
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div class="container">
+        <div class="left">
+          <h1>Select your attributes</h1>
+        </div>
+        <div class="center">
+          <h1>Appreciate your beauty</h1>
+        </div>
+        <div class="right">
+          <h1>Register your identity</h1>
+        </div>
+      </div>
     </div>
   );
 }
